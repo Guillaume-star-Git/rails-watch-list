345908efcc89ee393baf92b33268914b4e18de23
Guillaume Doré cd guillaume.dore@protonmail.com <guillaume.dore@protonmail.com>
2021-05-21 17:04:55 +0200

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  # seulement 3 actions
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [ :new, :create ]
-  end
-  resources :bookmarks, only: [ :destroy ]
+ root to: 'lists#index'
+ resources :lists, except: [:edit, :update] do
+   resources :bookmarks, only: [:new, :create]
+ end
+ resources :bookmarks, only: :destroy
 end
